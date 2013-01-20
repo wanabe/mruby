bf0a542eadf73f342971ac55065821a2237b259a
wanabe <s.wanabe@gmail.com>
2013-01-20 23:13:31 +0900

    Add code area management original commit by miura1729 https://github.com/miura1729/mruby/commit/529cf9d8d79ae7bbf2d500a5e358ff2b47eafcfa

--- src/jitcode.cc
+++ src/jitcode.cc
@@ -4,10 +4,21 @@ extern "C" {
 #include "mruby.h"
 #include "mruby/jit.h"
 
-void 
-emit_code(mrb_state *mrb, mrb_irep *irep, mrb_code **ppc, mrb_value *regs)
+#ifdef ENABLE_JIT
+
+mrbjit_code_area
+mrbjit_alloc_code()
 {
-  MRBJitCode code;
-  code.emit_mov();
+  return (mrbjit_code_area)(new MRBJitCode());
 }
+
+const void *
+mrbjit_emit_code(mrbjit_code_area coderaw, mrb_state *mrb, mrb_irep *irep, mrb_code **ppc, mrb_value *regs)
+{
+  MRBJitCode *code = (MRBJitCode *) coderaw;
+  return code->emit_mov();
+}
+
+#endif // ENABLE_JIT
+
 } /* extern "C" */
